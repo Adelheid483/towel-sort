70fa71b1f039fe158ad203f9e6992f49c31410dc
Alina Ivanova <58606840+Adelheid483@users.noreply.github.com>
2020-10-03 12:59:46 +0300

    fix: fix solution

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@
 
 module.exports = function towelSort (matrix) {
   let result = [];
-    
+  if (!matrix) return result;
   for (let i = 0; i < matrix.length; i++) {
     if (i % 2 !== 0) {
         matrix[i].reverse().forEach(item => result.push(item))
